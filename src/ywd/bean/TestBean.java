7ff7fdcc0dac417f2f313a035a76d127794e1ba3
ywd_aoc3 <alwayslookback@sina.com>
2016-09-30 00:23:44 +0800

    first commit

---
+++ src/ywd/bean/TestBean.java
@@ -0,0 +1,16 @@
+package ywd.bean;
+
+import java.util.Random;
+
+public class TestBean {
+    private String testStr = "Hello Spring!";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+
+}
